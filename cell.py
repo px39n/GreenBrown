99d39a70363edd6c6e11ee611440796077187d34
Zhehao liang <isxzl39@gmail.com>
2020-10-25 06:29:10 +0000

    delete vn

--- cell.py
+++
@@ -1,9 +0,0 @@
-from greenbrown.bfast.breakpoints import extract_bps
-from greenbrown.utils import load_example
-import numpy as np
-if __name__ == '__main__':
-    ndvi_s=load_example()
-    y=ndvi_s.values
-    x=np.arange(1, len(y) + 1)
-    a=extract_bps(y,5)
-    print(a)
